045d2b685e866e4fa6fe7a45648b3c47a61aad7d
caneppelevitor <vitor.a.caneppele@gmail.com>
2025-04-02 08:12:36 +0200

    minor changes

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
+output "instance_ocid" {
+  description = "The OCID of the created instance"
+  value       = oci_core_instance.example.id
+}
+
 output "public_ip" {
-  value = oci_core_instance.discourse_vm.public_ip
+  description = "The public IP of the instance"
+  value = oci_core_instance.example.public_ip
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,67 @@
+variable "tenancy_ocid" {
+  type        = string
+  description = "The OCID of your tenancy"
+}
 
-variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
-variable "region" {}
+variable "user_ocid" {
+  type        = string
+  description = "The OCID of the user calling the API"
+}
 
-variable "availability_domain" {}
-variable "compartment_ocid" {}
-variable "ssh_public_key" {}
+variable "fingerprint" {
+  type        = string
+  description = "The fingerprint for the user public key in OCI"
+}
+
+variable "private_key_path" {
+  type        = string
+  description = "Path to your private key for OCI API signing"
+}
+
+variable "region" {
+  type        = string
+  description = "OCI region to use (e.g., 'us-ashburn-1')"
+}
+
+variable "compartment_ocid" {
+  type        = string
+  description = "OCID of the compartment in which to create resources"
+}
+
+variable "availability_domain" {
+  type        = string
+  description = "The availability domain where the instance will launch"
+}
+
+variable "subnet_ocid" {
+  type        = string
+  description = "The OCID of the subnet where the instance's VNIC will reside"
+}
+
 variable "instance_shape" {
-  default = "VM.Standard.A1.Flex"
-}
-variable "image_ocid" {
-  description = "Ubuntu 22.04 image OCID for your region"
+  type        = string
+  description = "Shape of the instance (e.g., VM.Standard.E3.Flex)"
 }
 
+variable "image_ocid" {
+  type        = string
+  description = "OCID of the image to use for the instance"
+}
+
+variable "instance_display_name" {
+  type        = string
+  description = "A friendly name for the instance"
+  default     = "example-terraform-instance"
+}
+
+variable "hostname_label" {
+  type        = string
+  description = "Hostname for the instance"
+  default     = "example-host"
+}
+
+variable "ssh_public_key" {
+  type        = string
+  description = "Path to your SSH public key"
+  default     = "~/.ssh/id_rsa.pub"
+}
